45b2bff7a3542f19c9a72629ece595ac44860509
Suwapat Kittinanon <suwapat.k@outlook.co.th>
2018-01-16 12:13:46 +0700

    Merge All BU

--- src/sbl_nrtsale.py
+++ src/sbl_nrtsale.py
@@ -21,83 +21,24 @@ _time = datetime.strptime('2017-11-03 10:00', '%Y-%m-%d %H:%M')
 # time = datetime.now()
 
 def connect_db():
-  return pymssql.connect("10.17.221.173", "app-t1c", "Zxcv123!", "DBMKP")
+  return pymssql.connect("10.17.220.173", "app-t1c", "Zxcv123!", "DBMKP")
 
-def generate_text():
-  print("--- Begin: GenerateText ---")
-  shop_master = get_shop_master()
+def generate_text_t1c():
+  dt_batch_date_get()
+  sale_transaction(_file_name)
 
-  gen_text_bu(shop_master)
-  gen_text_indy(shop_master)
-  gen_text_mer(shop_master)
-
-  gen_ctrl_text()
-
-  print('--- End: GenerateText ---')
-
-def get_shop_master():
-  with connect_db() as conn:
-    cursor = conn.cursor(as_dict=True)
-    cursor.execute(
-        'SELECT ShopId, ShopGroup, AccountCode, BU FROM TBShopMaster')
-    return [{
-        'ShopId': str(shop['ShopId']),
-        'ShopGroup': shop['ShopGroup'],
-        'AccountCode': shop['AccountCode'],
-        'BU': shop['BU']
-    } for shop in cursor]
-
-def get_shop_master_by_bu_list():
-  with connect_db() as conn:
-    cursor = conn.cursor(as_dict=True)
-    cursor.execute(
-        'SELECT DISTINCT ShopGroup, AccountCode, BU FROM TBShopMaster WHERE BU IN (\''
-        + str.join('\',\'', enum_bu) + '\')')
-    return [{
-        'ShopGroup': shop['ShopGroup'],
-        'AccountCode': shop['AccountCode'],
-        'BU': shop['BU']
-    } for shop in cursor]
-
-def gen_text_bu(shop_master):
-  shop_master_by_bus = get_shop_master_by_bu_list()
-  bu_shops = [
-      shop for shop in shop_master
-      if (shop['ShopGroup'] in enum_shopgroup) & (shop['BU'] in enum_bu)
-  ]
-  for item in shop_master_by_bus:
-    bu_shop = str.join(',', [
-        bu['ShopId'] for bu in bu_shops
-        if bu['AccountCode'] == item['AccountCode']
-    ])
-    generate_text_t1c('CGO', item['AccountCode'], bu_shop, 'BU')
-
-def gen_text_indy(shop_master):
-  indy_shops = [shop for shop in shop_master if shop['ShopGroup'] == 'IN']
-  indy_shop = str.join(',', [indy['ShopId'] for indy in indy_shops])
-  generate_text_t1c('CGO', '99564', indy_shop, 'IN')
-
-def gen_text_mer(shop_master):
-  mer_shops = [shop for shop in shop_master if shop['ShopGroup'] == 'ME']
-  mer_shop = str.join(',', [mer['ShopId'] for mer in mer_shops])
-  generate_text_t1c('CGO', '99998', mer_shop, 'ME')
-
-def generate_text_t1c(bu, store_number, shop_id, shop_group):
-  dt_batch_date_get(store_number, bu)
-  sale_transaction(bu, store_number, shop_id, shop_group, _file_name)
-
-def dt_batch_date_get(store_number, bu):
+def dt_batch_date_get():
   global _file_name
   dt_batch_date = get_batch_date()
   month = '{:02}'.format(dt_batch_date.month)
   day = '{:02}'.format(dt_batch_date.day)
   year = str(dt_batch_date.year)[2:4]
-  _file_name = 'BCH_{}_T1C_NRTSales_{}{}{}.dat'.format(bu, year, month, day)
+  _file_name = 'BCH_CGO_T1C_NRTSales_{}{}{}.dat'.format(year, month, day)
 
-def sale_transaction(bu, store_number, shop_id, shop_group, file_name):
+def sale_transaction(file_name):
   print('--- Begin: SaleTransaction ---')
   global total_row
-  sale_transactions = get_sale_tran(shop_id, shop_group, store_number)
+  sale_transactions = get_sale_tran()
   total = len(sale_transactions)
   with open(CFG_SALES_TRANSACTION_PATH + file_name, 'a') as text_file:
     for transaction in sale_transactions:
@@ -106,114 +47,99 @@ def sale_transaction(bu, store_number, shop_id, shop_group, file_name):
   total_row = total_row+total
   print('--- End: SaleTransaction ---')
 
-def get_sale_tran(shop_id, shop_group, store_number):
+def get_sale_tran():
   with connect_db() as conn:
     cursor = conn.cursor(as_dict=True)
     query = """
-    SELECT
-        Head.ShopID,
-        (case when Head.shopGroup = 'ME' then Concat('0', substring(Head.subOrderId,1,12)) else Head.InvNo end) as InvNo,
-        Head.InvDate,
-        Head.CreateOn,
-        Head.PaymentDate as BusinessDate,
-        Head.DeliveryDate,
-        '01' AS SaleType,
-        Detail.PID,
-        Detail.Quantity,
-        Detail.UnitPrice ,
-        Detail.UnitPrice - (Detail.ItemDiscAmt + Detail.OrdDiscAmt) AS UnitSalesPrice,
-        Detail.SeqNo,
-        Head.VatAmt ,
-        (Detail.UnitPrice * Detail.Quantity) - (Detail.ItemDiscAmt + Detail.OrdDiscAmt) AS NetAmt,
-        (Head.ItemDiscAmt + Head.OrdDiscAmt) AS TransactionDiscountAmount ,
-        ProMas.ProdBarcode,
-        Head.T1CNoEarn,
-        Head.RedeemAmt,
-        Head.PaymentRefNo
-    FROM
-        TBSubOrderHead Head
-    INNER JOIN
-        TBSubOrderDetail Detail ON Head.Suborderid = Detail.Suborderid
-    LEFT JOIN
-        TBProductMaster ProMas ON Detail.PID = ProMas.PID
-    WHERE
-        Head.InvDate = CONVERT(VARCHAR(19),DateAdd(dd,-1,%(batch_date)s),111)
+        SELECT
+         S.AccountCode as StoreNo,
+         S.StroeCode as POSNo,
+         Head.ShopID,
+         Head.InvNo,
+         Head.InvDate,
+         Head.CreateOn,
+         Head.PaymentDate as BusinessDate,
+         Head.DeliveryDate,
+         '01' AS TransType,
+         Detail.PID,
+         Detail.Quantity,
+         Detail.UnitPrice ,
+         Detail.UnitPrice - (Detail.ItemDiscAmt + Detail.OrdDiscAmt) AS UnitSalesPrice,
+         Detail.SeqNo,
+         Head.VatAmt ,
+         (Detail.UnitPrice * Detail.Quantity) - (Detail.ItemDiscAmt + Detail.OrdDiscAmt) AS NetAmt,
+         (Head.ItemDiscAmt + Head.OrdDiscAmt) AS TransactionDiscountAmount ,
+         ProMas.ProdBarcode,
+         Head.T1CNoEarn,
+         Head.RedeemAmt,
+         Head.PaymentRefNo,
+         Head.TrackingId as DisplayReceipt
+        FROM TBSubOrderHead Head
+        INNER JOIN TBShopMaster S on S.ShopID = Head.ShopID
+        INNER JOIN TBSubOrderDetail Detail ON Head.Suborderid = Detail.Suborderid
+        LEFT JOIN TBProductMaster ProMas ON Detail.PID = ProMas.PID
+        WHERE cast(Head.InvDate as date) = cast(getdate() - 1 as date)
         AND Head.InvNo != ''
-        AND head.ShopGroup = %(ShopGroup)s {0}
 
-    UNION ALL
+        UNION ALL
 
-    SELECT
-        Head.ShopID,
-        Head.CnNo AS InvNo,
-        CONVERT(VARCHAR(19),Head.CnDate,111) AS InvDate,
-        Head.CreateOn,
-        Head.CnDate as BusinessDate,
-        Head.CnDate as DeliveryDate,
-        '07' AS SaleType,
-        Detail.PID,
-        Detail.Quantity,
-        Detail.UnitPrice ,
-        Detail.UnitPrice - (Detail.ItemDiscAmt + Detail.OrdDiscAmt) AS UnitSalesPrice,
-        Detail.SeqNo,
-        Head.VatAmt ,
-        (Detail.UnitPrice * Detail.Quantity) - (Detail.ItemDiscAmt + Detail.OrdDiscAmt) AS NetAmt,
-        (Head.ItemDiscAmt + Head.OrdDiscAmt) AS TransactionDiscountAmount ,
-        ProMas.ProdBarcode,
-        Head.T1CNoEarn,
-        Head.RedeemAmt,
-        Head.PaymentRefNo
-    FROM
-        TBSubSaleReturnHead Head
-    INNER JOIN
-        TBSubSaleReturnDetail Detail ON Head.SubSRNo = Detail.SubSRNo
-    LEFT JOIN
-        TBProductMaster ProMas ON Detail.PID = ProMas.PID
-    WHERE
-        Head.SubSaleReturnType IN ('CN', 'Exchange')
+        SELECT
+         S.AccountCode as StoreNo,
+         S.StroeCode as POSNo,
+         Head.ShopID,
+         Head.CnNo AS InvNo,
+         CONVERT(VARCHAR(19),Head.CnDate,111) AS InvDate,
+         Head.CreateOn,
+         Head.CnDate as BusinessDate,
+         Head.CnDate as DeliveryDate,
+         '07' AS TransType,
+         Detail.PID,
+         Detail.Quantity,
+         Detail.UnitPrice ,
+         Detail.UnitPrice - (Detail.ItemDiscAmt + Detail.OrdDiscAmt) AS UnitSalesPrice,
+         Detail.SeqNo,
+         Head.VatAmt ,
+         (Detail.UnitPrice * Detail.Quantity) - (Detail.ItemDiscAmt + Detail.OrdDiscAmt) AS NetAmt,
+         (Head.ItemDiscAmt + Head.OrdDiscAmt) AS TransactionDiscountAmount ,
+         ProMas.ProdBarcode,
+         Head.T1CNoEarn,
+         Head.RedeemAmt,
+         Head.PaymentRefNo,
+         Head.SubSRNo as DisplayReceipt
+        FROM TBSubSaleReturnHead Head
+        INNER JOIN TBShopMaster S on S.ShopID = Head.ShopID
+        INNER JOIN TBSubSaleReturnDetail Detail ON Head.SubSRNo = Detail.SubSRNo
+        LEFT JOIN TBProductMaster ProMas ON Detail.PID = ProMas.PID
+        WHERE Head.SubSaleReturnType IN ('CN', 'Exchange')
         AND Head.Status = 'Complete'
         AND Head.CnNo != ''
-        AND CONVERT(VARCHAR(19),Head.CnDate,111) =CONVERT(VARCHAR(19),DateAdd(dd,-1,%(batch_date)s),111)
-        AND Head.ShopGroup = %(ShopGroup)s {0}
-    ORDER BY
-        InvNo
+        AND cast(Head.InvDate as date) = cast(getdate() - 1 as date)
+        ORDER BY InvNo
     """
-    if shop_group == 'BU':
-      query = query.format('{} ({})'.format(' AND Head.ShopID in ', shop_id))
-    else:
-      query = query.format('')
-    cursor.execute(query,
-                   dict(ShopGroup=shop_group, batch_date=get_sql_batch_date()))
+    cursor.execute(query)
     return [
-        gen_sale_tran_data(data, index + 1, store_number)
+        gen_sale_tran_data(data, index + 1 )
         for index, data in enumerate(cursor)
     ]
 
 
-def gen_sale_tran_data(data, index, store_number):
-  store_number = store_number.zfill(5)
+def gen_sale_tran_data(data, index):
+  store_number = get_option_str(data['StoreNo'])
 
   inv_no = get_option_str(data['InvNo'])
   bu = inv_no[0:3]
 
   source_trans_id = partner_code + store_number + datetime.strptime(data['InvDate'],
                                          '%Y-%m-%d').strftime('%Y%m%d')
-  if data['InvDate'] == None:
-    transaction_date = '        '
-  else:
-    transaction_date = datetime.strptime(data['InvDate'],
+  transaction_date = datetime.strptime(data['InvDate'],
                                          '%Y-%m-%d').strftime('%Y%m%d')
 
-  if data['CreateOn'] == None:
-    transaction_time = '    '
-  else:
-    transaction_time = data['CreateOn'].strftime('%H%M')
+  transaction_time = data['CreateOn'].strftime('%H%M')
 
-  trans_type = get_option_str(data['SaleType'])
+  trans_type = get_option_str(data['TransType'])
   trans_date = transaction_date + transaction_time
 
-  pos_number = get_option_str(data['ShopID'])
-  pos_number = '{:0>4}'.format(pos_number)
+  pos_number = get_option_str(data['POSNo'])
 
   ticket_running_number = get_tracking_number(inv_no)
   receipt_number = store_number + pos_number + ticket_running_number \
@@ -224,8 +150,6 @@ def gen_sale_tran_data(data, index, store_number):
   delivery_date = get_option_str(data['DeliveryDate'])
   earn_online_flag = 'N'
   t1c_card_no = get_option_str(data['T1CNoEarn'])
-  t1c_card_no = '{: <16}'.format(t1c_card_no) if t1c_card_no == None else '{: <16}'.format(
-          t1c_card_no) if len(t1c_card_no) < 16 else t1c_card_no[0:16]
   redeem_amt = get_option_str(data['RedeemAmt'])
   trans_sub_type = 'T' if t1c_card_no != '' or redeem_amt != "0.000'" else 'P'
 
@@ -253,6 +177,7 @@ def gen_sale_tran_data(data, index, store_number):
   tender_ref_no = '' if trans_sub_type == 'P' else t1c_card_no if redeem_amt != '0.000' else get_option_str(data['PaymentRefNo'])
   original_receipt_no = ''
   original_item_seq_no = get_option_str(data['SeqNo'])
+  display_receipt_no = get_option_str(data['DisplayReceipt'])
 
   res = []
   res.append('1')
@@ -284,7 +209,8 @@ def gen_sale_tran_data(data, index, store_number):
   res.append(tender_ref_no)
   res.append(original_receipt_no)
   res.append(original_item_seq_no)
-  # + display_receipt_no + return_all_flag + sblc_ncl_redeem_txn_id
+  res.append(display_receipt_no)
+  # + return_all_flag + sblc_ncl_redeem_txn_id
   # if len(raw_sale_tran) == 281:
   return "|".join(res)
 
@@ -334,4 +260,4 @@ def create_directory(path):
 
 if __name__ == "__main__":
   create_directory('SaleTransaction')
-  generate_text()
+  generate_text_t1c()
